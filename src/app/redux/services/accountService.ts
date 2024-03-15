7b8f44195f0d0ad99e9bd74b80e5d2540868b641
Oleh H <oleh.husiev.dev@gmail.com>
2024-03-15 23:59:27 +0100

    Add service

---
+++ src/app/redux/services/accountService.ts
@@ -0,0 +1,9 @@
+import { createBaseApi } from '@/app/redux/api';
+
+export const ACCOUNT_SERVICE_REDUCER_KEY = 'accountService';
+
+const baseApi = createBaseApi(ACCOUNT_SERVICE_REDUCER_KEY);
+
+export const accountService = baseApi.injectEndpoints({
+  endpoints: (build) => ({})
+})
