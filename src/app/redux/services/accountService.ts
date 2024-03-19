c98cced8114844875f12bf75b6b6ae037782366e
Oleh H <oleh.husiev.dev@gmail.com>
2024-03-20 00:10:07 +0100

    Add a new endpoint for creating account

--- src/app/redux/services/accountService.ts
+++ src/app/redux/services/accountService.ts
@@ -1,18 +1,31 @@
-import { createBaseApi } from '@/app/redux/api';
+import { createBaseApi, ErrorResponseType } from '@/app/redux/api';
 
 export const ACCOUNT_SERVICE_REDUCER_KEY = 'accountService';
 
 const baseApi = createBaseApi(ACCOUNT_SERVICE_REDUCER_KEY);
 
-interface CheckAccountResponse {}
+interface AccountResponse {
 
+}
+interface CreateAccountBody {
+  id: string;
+  name: string;
+  photoUrl: string;
+}
 
 export const accountService = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    checkAccount: builder.query<CheckAccountResponse, string>({
-      query: (id: string) => `/account/check?id=${id}`
+    checkAccount: builder.query<AccountResponse | ErrorResponseType, string>({
+      query: (id: string) => `/account/getAccount?id=${id}`
+    }),
+    createAccount: builder.mutation({
+      query: (body: CreateAccountBody) => ({
+        url: '/account/createAccount',
+        method: 'POST',
+        body: body
+      })
     })
   })
 })
 
-export const { useCheckAccountQuery, useLazyCheckAccountQuery } = accountService;
+export const { useLazyCheckAccountQuery, useCreateAccountMutation } = accountService;
