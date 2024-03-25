ada4d308aab92f89a8847f3113ac97dfed841e41
Oleh H <oleh.husiev.dev@gmail.com>
2024-03-25 22:32:49 +0100

    Update account component

--- src/app/components/shared/Account/Account.tsx
+++ src/app/components/shared/Account/Account.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown, ChevronUp } from 'lucide-react';
 import { getAccount } from '@/app/redux/features';
+import { Popover, PopoverContent, PopoverTrigger } from '@/app/components';
 
 
 export const Account = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const accountDetails = useSelector(getAccount);
   return (
     <div className="flex">
-      <div className="flex gap-2 bg-tabBG py-2 px-5 rounded-lg">
-        <div className="flex gap-1 items-center">
-          <p className="font-bold text-sm">{accountDetails.credits}</p>
-          <p className="text-sm">credits</p>
-        </div>
-        <div className="w-[32px] h-[25px] overflow-hidden rounded-sm ml-2.5">
-          <img src={accountDetails.photoUrl} alt={accountDetails.name}/>
-        </div>
-        <ChevronDown className="stroke-input"/>
-      </div>
+      <Popover onOpenChange={setIsOpen}>
+        <PopoverTrigger>
+          <div className="flex gap-2 bg-tabBG py-2 px-5 rounded-lg">
+            <div className="flex gap-1 items-center">
+              <p className="font-bold text-sm">{accountDetails.credits}</p>
+              <p className="text-sm">credits</p>
+            </div>
+            <div className="w-[32px] h-[25px] overflow-hidden rounded-sm ml-2.5">
+              <img src={accountDetails.photoUrl} alt={accountDetails.name}/>
+            </div>
+            {isOpen ? (
+              <ChevronUp className="stroke-input"/>
+              ) : (
+              <ChevronDown className="stroke-input"/>
+            )}
+          </div>
+        </PopoverTrigger>
+        <PopoverContent>Place content for the popover here.</PopoverContent>
+      </Popover>
+
     </div>
 
   )
