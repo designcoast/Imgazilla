25f1474f7b6ff56ce0f850623b5a6b7817fde0d3
Oleh H <oleh.husiev.dev@gmail.com>
2024-03-26 22:21:32 +0100

    Update some component and add sheet component

--- src/app/components/ui/Switch/Switch.tsx
+++ src/app/components/ui/Switch/Switch.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import * as SwitchPrimitives from "@radix-ui/react-switch"
+import React from 'react';
+import * as SwitchPrimitives from '@radix-ui/react-switch';
 
 import { cn } from '@/app/lib/utils';
 
