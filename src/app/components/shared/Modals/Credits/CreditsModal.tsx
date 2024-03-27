4b7ee3db4956c59c681f8f916459fa2af7c1dd07
Oleh H <oleh.husiev.dev@gmail.com>
2024-03-27 23:53:12 +0100

    Add Credits component

---
+++ src/app/components/shared/Modals/Credits/CreditsModal.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@/app/components';
+
+export const Credits = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant='ghost'>Instructions</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className="w-[80%]">
+          <DialogTitle className="text-left text-lg">Need Help?</DialogTitle>
+          <DialogDescription>Feel free to reach out to us through our public Discord channel.</DialogDescription>
+        </DialogHeader>
+
+      </DialogContent>
+    </Dialog>
+  )
+};
