90acccd57085a7d3f5e7927cef08be26d2983a81
Oleh H <oleh.husiev.dev@gmail.com>
2024-04-01 00:42:27 +0200

    Add images preview from collection

--- src/app/components/shared/ImageOptimization/ImageOptimization.tsx
+++ src/app/components/shared/ImageOptimization/ImageOptimization.tsx
@@ -1,11 +1,15 @@
+import React from 'react';
 import { useWindowMessaging } from '@/app/hooks/useFigmaMessaging';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { EventType, UIEventType } from '@/eventType';
+import { convertToImageUrl } from '@/app/lib/convertToImageUrl';
 
 export const ImageOptimization = () => {
+  const [data, setData] = useState([])
   const handleFigmaPluginMessages = useCallback((message: MessageType) => {
     if (message.type === EventType.IMAGES_UINT_ARRAY_COLLECTION) {
-      console.log(message.payload)
+      console.log(message.payload.data)
+      setData(message.payload.data);
     }
   }, []);
 
@@ -19,6 +23,14 @@ export const ImageOptimization = () => {
   }, []);
 
   return (
-    "image optimization"
+    <>
+      {data.map((item, index) => {
+        const img = convertToImageUrl(item);
+
+        return (
+          <img key={index} src={img} alt={`alt-${index}`}/>
+        )
+      })}
+    </>
   )
 }
