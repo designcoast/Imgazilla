6fe3d4c0c494030e36c0eca4cba3a32b4d69504c
Oleh H <oleh.husiev.dev@gmail.com>
2024-03-31 23:08:30 +0200

    Add functionality for getting image collection from Plugin

--- src/app/components/shared/ImageOptimization/ImageOptimization.tsx
+++ src/app/components/shared/ImageOptimization/ImageOptimization.tsx
@@ -1,4 +1,23 @@
+import { useWindowMessaging } from '@/app/hooks/useFigmaMessaging';
+import { useCallback, useEffect } from 'react';
+import { EventType, UIEventType } from '@/eventType';
+
 export const ImageOptimization = () => {
+  const handleFigmaPluginMessages = useCallback((message: MessageType) => {
+    if (message.type === EventType.IMAGES_UINT_ARRAY_COLLECTION) {
+      console.log(message.payload)
+    }
+  }, []);
+
+  const { onSendMessage } = useWindowMessaging(handleFigmaPluginMessages);
+
+  useEffect(() => {
+    onSendMessage({
+      type: UIEventType.GET_IMAGES_UINT_ARRAY_COLLECTION,
+      payload: {}
+    })
+  }, []);
+
   return (
     "image optimization"
   )
