8277fa2dd84acdebcfd3cec6c81c57ae68adae26
Oleh H <oleh.husiev.dev@gmail.com>
2024-03-31 23:07:43 +0200

    Update events, and add UI events

--- src/eventType.ts
+++ src/eventType.ts
@@ -2,5 +2,10 @@ export enum EventType {
   MULTIPLE_NODES_SELECTED_ERROR = 'multiple_nodes_selected_error',
   NON_SQUARE_NODE_SELECTED_ERROR = 'non_square_node_selected_error',
   IMAGE_UNIT_ARRAY_DATA = 'image_unit_array_data',
-  USER_ACCOUNT_DATA = 'user_account_data'
+  USER_ACCOUNT_DATA = 'user_account_data',
+  IMAGES_UINT_ARRAY_COLLECTION = 'images_uint_array_collection',
+}
+
+export enum UIEventType {
+  GET_IMAGES_UINT_ARRAY_COLLECTION = 'get_images_uint_array_collection',
 }
