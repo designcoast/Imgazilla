1f2523a5d08249b668b22663bd913af755f3e71e
Oleh H <oleh.husiev.dev@gmail.com>
2024-04-03 23:52:45 +0200

    Add loading state for image optimization slice and component

--- src/app/components/shared/ImageOptimization/ImageOptimization.tsx
+++ src/app/components/shared/ImageOptimization/ImageOptimization.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 import { useWindowMessaging } from '@/app/hooks/useFigmaMessaging';
-import { useCallback, useEffect } from 'react';
 import { EventType, UIEventType } from '@/eventType';
 import { convertToImageUrl } from '@/app/lib/convertToImageUrl';
 import { useTypedDispatch } from '@/app/redux/store';
 import { getImages, setImagesForOptimization } from '@/app/redux/features';
-import { useSelector } from 'react-redux';
 
 export const ImageOptimization = () => {
   const dispatch = useTypedDispatch();
 
-  const images = useSelector(getImages);
+  const { images, isLoading } = useSelector(getImages);
 
   const handleFigmaPluginMessages = useCallback((message: MessageType) => {
     if (message.type === EventType.IMAGES_UINT_ARRAY_COLLECTION) {
@@ -30,6 +30,12 @@ export const ImageOptimization = () => {
       payload: {}
     })
   }, [images]);
+
+  if (isLoading) {
+    return (
+      <div>Loading</div>
+    )
+  }
 
   return (
     <>
--- src/app/redux/features/optimizationImageSlice.ts
+++ src/app/redux/features/optimizationImageSlice.ts
@@ -1,24 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '@/app/redux/store';
 
 interface ImagesState {
   images: Uint8Array[];
+  isLoading: boolean;
 }
 
 const initialState = {
   images: [],
+  isLoading: true,
 } satisfies ImagesState as ImagesState;
 
 export const optimizationImageSlice = createSlice({
-  name: 'images',
+  name: 'optimizationImages',
   initialState,
   reducers: {
     setImagesForOptimization(state, action: PayloadAction<Uint8Array[]>) {
       state.images = action.payload
+      state.isLoading = false;
     },
   }
 });
 
-export const getImages = (state: ImagesState) => state.images;
+export const getImages = (state: RootState) => state.optimizationImages;
 
 export const { setImagesForOptimization } = optimizationImageSlice.actions;
