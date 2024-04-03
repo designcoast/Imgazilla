c127a0d649e56c79ea7e43bc9a2485c98b7f7904
Oleh H <oleh.husiev.dev@gmail.com>
2024-04-03 23:35:51 +0200

    Add slice for storing images for optimization

---
+++ src/app/redux/features/optimizationImageSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+interface ImagesState {
+  images: Uint8Array[];
+}
+
+const initialState = {
+  images: [],
+} satisfies ImagesState as ImagesState;
+
+export const optimizationImageSlice = createSlice({
+  name: 'images',
+  initialState,
+  reducers: {
+    setImagesForOptimization(state, action: PayloadAction<Uint8Array[]>) {
+      state.images = action.payload
+    },
+  }
+});
+
+export const { setImagesForOptimization } = optimizationImageSlice.actions;
