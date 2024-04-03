cdee24868b8aa3a40a5bcecaaa4c46cefd611127
Oleh H <oleh.husiev.dev@gmail.com>
2024-04-03 23:41:15 +0200

    Update component for getting images from global store

--- src/app/components/shared/ImageOptimization/ImageOptimization.tsx
+++ src/app/components/shared/ImageOptimization/ImageOptimization.tsx
@@ -1,30 +1,39 @@
 import React from 'react';
 import { useWindowMessaging } from '@/app/hooks/useFigmaMessaging';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 import { EventType, UIEventType } from '@/eventType';
 import { convertToImageUrl } from '@/app/lib/convertToImageUrl';
+import { useTypedDispatch } from '@/app/redux/store';
+import { getImages, setImagesForOptimization } from '@/app/redux/features';
+import { useSelector } from 'react-redux';
 
 export const ImageOptimization = () => {
-  const [data, setData] = useState([]);
+  const dispatch = useTypedDispatch();
+
+  const images = useSelector(getImages);
+
   const handleFigmaPluginMessages = useCallback((message: MessageType) => {
     if (message.type === EventType.IMAGES_UINT_ARRAY_COLLECTION) {
-      console.log(message.payload.data)
-      setData(message.payload.data);
+      dispatch(setImagesForOptimization(message.payload.data));
     }
   }, []);
 
   const { onSendMessage } = useWindowMessaging(handleFigmaPluginMessages);
 
   useEffect(() => {
+    if (images.length > 0) {
+      return
+    }
+
     onSendMessage({
       type: UIEventType.GET_IMAGES_UINT_ARRAY_COLLECTION,
       payload: {}
     })
-  }, []);
+  }, [images]);
 
   return (
     <>
-      {data.map((item, index) => {
+      {images.map((item, index) => {
         const img = convertToImageUrl(item);
 
         return (
