9a338f34d0db75b2315800a561c460303c2a4767
Oleh H <oleh.husiev.dev@gmail.com>
2024-04-19 00:12:58 +0200

    Add a new event

--- src/eventType.ts
+++ src/eventType.ts
@@ -4,6 +4,7 @@ export enum EventType {
   IMAGE_UNIT_ARRAY_DATA = 'image_unit_array_data',
   USER_ACCOUNT_DATA = 'user_account_data',
   IMAGES_UINT_ARRAY_COLLECTION = 'images_uint_array_collection',
+  OPEN_IMAGES_OPTIMIZATION_TAB = 'open_images_optimization_tab',
 }
 
 export enum UIEventType {
