707d70fe567ec5923d21c972be82e9552c354669
Oleh H <oleh.husiev.dev@gmail.com>
2024-04-18 21:06:48 +0200

    Update component which shows image optimization item

--- src/app/components/shared/ImageOptimization/ImageOptimizationItem/ImageOptimizationItem.tsx
+++ src/app/components/shared/ImageOptimization/ImageOptimizationItem/ImageOptimizationItem.tsx
@@ -1,16 +1,29 @@
 import React, { memo } from 'react';
-// import { convertToImageUrl } from '@/app/lib/convertToImageUrl';
+import { convertToImageUrl } from '@/app/lib/convertToImageUrl';
 
 type Props = {
   item: ImageInfo
 }
 
-export const ImageOptimizationItem = memo(({ item }: Props) => {
-  console.log('item', item);
+export const ImageOptimizationItem = memo(({ item: {
+  name,
+  width,
+  height,
+  uintArray
+} }: Props) => {
+  const imageUrl = convertToImageUrl(uintArray);
   return (
-    <div className="flex">item 2</div>
+    <div className="flex items-center gap-5 py-2.5">
+      <div>check</div>
+      <div className="flex max-w-[48px] max-h-[48px] justify-center items-center preview">
+        <img src={imageUrl} alt={name} className="rounded-md w-full h-full" />
+      </div>
+      <div className="flex">
+        {name}
+      </div>
+      <div className="flex">
+        {width}x{height}
+      </div>
+    </div>
   )
 })
-
-// const img = convertToImageUrl(item);
-//<img key={index} src={img} alt={`alt-${index}`}/>
