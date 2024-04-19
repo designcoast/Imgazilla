ae68c0cc71048017296d819d95c0590f2f72dfed
Oleh H <oleh.husiev.dev@gmail.com>
2024-04-19 16:39:22 +0200

    Update name of tab

--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -7,23 +7,26 @@ import {
   FaviconExporter,
   ImageOptimization, Account,
 } from '@/app/components';
+import { getSelectedTab } from '@/app/redux/features';
+import { useSelector } from 'react-redux';
 
 export const RootLayout = () => {
+  const activeTab = useSelector(getSelectedTab);
   return (
     <>
       <div className='flex justify-end p-2.5'>
         <Account />
       </div>
-      <Tabs defaultValue="favicon" className="w-full">
+      <Tabs defaultValue={activeTab} className="w-full">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="favicon">Favicon exporter</TabsTrigger>
-          <TabsTrigger value="image">Image optimisation</TabsTrigger>
+          <TabsTrigger value="imagesOptimization">Image optimisation</TabsTrigger>
         </TabsList>
         <>
           <TabsContent value="favicon">
             <FaviconExporter />
           </TabsContent>
-          <TabsContent value="image">
+          <TabsContent value="imagesOptimization">
             <ImageOptimization />
           </TabsContent>
         </>
