4b77a7fc95774844986bb2de447e5fe5b578f572
Oleh H <oleh.husiev.dev@gmail.com>
2024-04-21 22:06:21 +0200

    Update image optimization slice

--- src/app/redux/features/optimizationImageSlice.ts
+++ src/app/redux/features/optimizationImageSlice.ts
@@ -4,13 +4,13 @@ import { RootState } from '@/app/redux/store';
 
 interface ImagesState {
   images: ImageInfo[];
-  selectedImages: ImageInfo[];
+  generalOptimizationPercent: number;
   isLoading: boolean;
 }
 
 const initialState = {
   images: [],
-  selectedImages: [],
+  generalOptimizationPercent: 100,
   isLoading: true,
 } satisfies ImagesState as ImagesState;
 
@@ -18,14 +18,23 @@ export const optimizationImageSlice = createSlice({
   name: 'optimizationImages',
   initialState,
   reducers: {
-    setSelectedImages(state, action: PayloadAction<ImageInfo[]>) {
-      state.selectedImages = [
-        ...state.selectedImages,
-        ...action.payload
-      ]
+    setSelectedImages(state, action: PayloadAction<{ uuid: string }>) {
+      const index = state.images.findIndex(item => item.uuid === action.payload.uuid);
+      if (index !== -1) {
+        state.images[index] = {
+          ...state.images[index],
+          isSelected: true
+        };
+      }
     },
-    removeSelectedImages(state, action: PayloadAction<string>) {
-      state.selectedImages = state.selectedImages.filter(i => i.uuid !== action.payload);
+    removeSelectedImages(state, action: PayloadAction<{ uuid: string }>) {
+      const index = state.images.findIndex(item => item.uuid === action.payload.uuid);
+      if (index !== -1) {
+        state.images[index] = {
+          ...state.images[index],
+          isSelected: false
+        };
+      }
     },
     setImagesForOptimization(state, action: PayloadAction<ImageInfo[]>) {
       state.images = [
@@ -34,10 +43,52 @@ export const optimizationImageSlice = createSlice({
       ]
       state.isLoading = false;
     },
+    updateGeneralOptimizationPercent(state, action: PayloadAction<number>) {
+      state.generalOptimizationPercent = action.payload;
+    },
+    updateImageOptimizationPercent(state, action: PayloadAction<{uuid: string, percent: number}>) {
+      state.images = state.images.map((item) => {
+        if(item.uuid === action.payload.uuid) {
+          return {
+            ...item,
+            optimizationPercent: action.payload.percent
+          }
+        }
+        return item;
+      })
+    },
+    unselectAllImages(state) {
+      state.images = state.images.map((item) => {
+        return {
+          ...item,
+          isSelected: false
+        }
+      })
+    },
+    selectAllImages(state) {
+      state.images = state.images.map((item) => {
+        return {
+          ...item,
+          isSelected: true
+        }
+      })
+    }
   }
 });
 
-export const getImages = (state: RootState) => state.optimizationImages;
-export const getSelectedImages = (state: RootState) => state.optimizationImages.selectedImages;
+export const getImages = (state: RootState) => state.optimizationImages.images;
+export const getIsLoading = (state: RootState) => state.optimizationImages.isLoading;
 
-export const { setImagesForOptimization, setSelectedImages, removeSelectedImages } = optimizationImageSlice.actions;
+export const getSelectedImagesCount = (state: RootState) =>
+  state.optimizationImages.images.filter((item) => item.isSelected).length;
+export const getGeneralOptimizationPercent = (state: RootState) => state.optimizationImages.generalOptimizationPercent;
+
+export const {
+  setImagesForOptimization,
+  setSelectedImages,
+  removeSelectedImages,
+  updateGeneralOptimizationPercent,
+  updateImageOptimizationPercent,
+  unselectAllImages,
+  selectAllImages,
+} = optimizationImageSlice.actions;
