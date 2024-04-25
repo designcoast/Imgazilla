b43c7d86df335fb614ee0284eb658a44d7cdd2b1
Oleh H <oleh.husiev.dev@gmail.com>
2024-04-25 19:39:46 +0200

    Add a title for optimization level

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,9 @@
 export const DEFAULT_OPTIMIZATION_PERCENTAGE = 100;
+
+export const TITLE_TO_QUALITY_PERCENTAGE = {
+  100: "Highest Quality",
+  75: "High Quality",
+  50: "Medium Quality",
+  25: "Basic Quality",
+  0: "Low Quality",
+};
