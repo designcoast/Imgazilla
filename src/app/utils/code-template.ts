8777926280b0ede437e0a573131fadd6dfea8d97
Oleh H <oleh.husiev.dev@gmail.com>
2024-05-06 23:42:30 +0200

    Update code templates

--- src/app/utils/code-template.ts
+++ src/app/utils/code-template.ts
@@ -1,11 +1,5 @@
 import escapeHtml from 'escape-html';
 
-type ManifestIcon = {
-  src: string,
-  sizes: string,
-  type: string
-};
-
 const appleHtmlSnippet = `
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
@@ -49,17 +43,17 @@ export const getHtmlSnippet = ({
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">`
   );
 
-  if (isAndroid && !websiteName) {
+  if (isAndroid) {
     snippet.push(
       `
-      <link rel="manifest" href="/site.webmanifest">`
+    <link rel="manifest" href="manifest.json" />`
     )
   }
 
   if (isAndroid && websiteName) {
     snippet.push(
       `
-      <meta name="application-name" content="${escapeHtml(websiteName)}">`
+    <meta name="application-name" content="${escapeHtml(websiteName)}">`
     )
   }
 
@@ -71,49 +65,67 @@ export const getHtmlSnippet = ({
   return snippet
 };
 
-
-// `
-//
-//     ${isIOS ? appleHtmlSnippent : ''}
-//     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-//     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-//     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
-//     ${isAndroid && '<link rel="manifest" href="/site.webmanifest">'}
-//     ${isAndroid && websiteName && `<meta name="application-name" content="${escapeHtml(websiteName)}">`}
-//     <meta name="theme-color" content="${color || '#FFFFFF'}">
-//  `
-
-
 export const getManifestObject = ({
-  name,
-  themeColor,
-  icons
+  websiteName,
+  themeColor
 }: {
-  name: string,
+  websiteName: string,
   themeColor: string
-  icons: ManifestIcon[]
 }) => ({
- 'short_name': 'App',
- 'name': name,
- 'icons': icons.map((icon: ManifestIcon) => ({
-   src: icon.src,
-   sizes: icon.sizes,
-   type: icon.type || 'image/png'
-
- })),
- 'start_url': 'index.html',
- 'display': 'standalone',
- 'theme_color': themeColor,
- 'background_color': '#FFFFFF'
+  'short_name': websiteName,
+  'name': websiteName,
+  'description': 'Manifest description',
+  'start_url': 'index.html',
+  'display': 'standalone',
+  'theme_color': themeColor,
+  'background_color': '#FFFFFF',
+  "icons": [
+    {
+      "src": "icons/android-chrome-36x36.png",
+      "type": "image/png",
+      "sizes": "36x36"
+    },
+    {
+      "src": "icons/android-chrome-48x48.png",
+      "type": "image/png",
+      "sizes": "48x48"
+    },
+    {
+      "src": "icons/android-chrome-72x72.png",
+      "type": "image/png",
+      "sizes": "72x72"
+    },
+    {
+      "src": "icons/android-chrome-96x96.png",
+      "type": "image/png",
+      "sizes": "96x96"
+    },
+    {
+      "src": "icons/android-chrome-144x144.png",
+      "type": "image/png",
+      "sizes": "144x144"
+    },
+    {
+      "src": "icons/android-chrome-192x192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "icons/android-chrome-256x256.png",
+      "type": "image/png",
+      "sizes": "256x256"
+    },
+    {
+      "src": "icons/android-chrome-384x384.png",
+      "type": "image/png",
+      "sizes": "384x384"
+    },
+    {
+      "src": "icons/android-chrome-512x512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    },
+  ],
 });
 
 export const headTag = `<head />`;
-
-
-// [
-//   {
-//     "src": "favicon.ico",
-//     "sizes": "64x64 32x32 24x24 16x16",
-//     "type": "image/x-icon"
-//   }
-// ]
