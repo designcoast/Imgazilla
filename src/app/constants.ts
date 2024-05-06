86d50d664a0712178080628c3381c423c00ec2a0
Oleh H <oleh.husiev.dev@gmail.com>
2024-05-06 23:42:41 +0200

    Add a new constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -9,3 +9,6 @@ export const TITLE_TO_QUALITY_PERCENTAGE = {
   25: "Basic Quality",
   0: "Low Quality",
 };
+
+//TODO: Return this options from the config API
+export const ANDROID_ICONS_SIZES = ['36x36', '48x48', '72x72', '96x96', '144x144', '192x192']
