38fdc5d9714f3b9fbd527d06ead120cdc5645ebd
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-01 01:28:33 +0200

    Update function for compressing data

--- src/app/lib/compressData.ts
+++ src/app/lib/compressData.ts
@@ -1,16 +1,11 @@
-// import pako from 'pako';
-const uint8ArrayToBase64 = (data: Uint8Array): string => {
-  return btoa(
-    data.reduce((acc, current) => acc + String.fromCharCode(current), "")
-  );
-}
+import { encode } from 'base64-arraybuffer-es6';
 
 export const transformAndCompressData = (data: ImageInfo[]) => {
   return data.map(item => ({
     uuid: item.uuid,
-    extension: item.extension,
+    format: item.format,
     name: item.name,
     optimizationPercent: item.optimizationPercent,
-    base64Image: uint8ArrayToBase64(item.uintArray),
+    base64Image: encode(item.uintArray),
   }));
 }
