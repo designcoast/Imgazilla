0a08fcb5fb41909ae9c84e4a0764eff4aacdf774
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-01 01:29:40 +0200

    Add a new function for generation archive

--- src/app/lib/generateArchive.ts
+++ src/app/lib/generateArchive.ts
@@ -1,4 +1,5 @@
 import JSZip from 'jszip';
+import { decode } from 'base64-arraybuffer-es6';
 import { getHtmlSnippet, getManifestObject } from '@/app/utils/code-template';
 
 export interface ImageObject {
@@ -46,6 +47,20 @@ export const generateArchive = async ({
   const html = getHtmlSnippet({ isAndroid, color: themeColor, websiteName, isIOS });
 
   zip.file('head.html', html.join(''))
+
+  return await zip.generateAsync({ type: 'blob', compressionOptions: { level: 9 }});
+}
+
+export const generateImagesArchive = async (images: ImageOptimizationResult[]) => {
+  const zip = new JSZip();
+
+  for (const image of images) {
+    const { name, base64Image } = image;
+
+    const buffer = decode(base64Image);
+
+    zip.file(name, buffer);
+  }
 
   return await zip.generateAsync({ type: 'blob', compressionOptions: { level: 9 }});
 }
