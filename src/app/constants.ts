6f6acd787acdf18f980c23a204bdb655e627e215
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-02 14:58:37 +0200

    Update consts for mime types

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -14,4 +14,10 @@ export const TITLE_TO_QUALITY_PERCENTAGE = {
 //TODO: Return this options from the config API
 export const ANDROID_ICONS_SIZES = ['36x36', '48x48', '72x72', '96x96', '144x144', '192x192']
 
-export const MIME_TYPE = 'image/png';
+export const MIME_TYPE_PNG = 'image/png';
+export const MIME_TYPE_SVG = 'image/svg+xml';
+
+export const FORMAT_TO_MIME_TYPE = {
+  'PNG': MIME_TYPE_PNG,
+  'SVG': MIME_TYPE_SVG,
+}
