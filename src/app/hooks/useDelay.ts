fde0afb35c88403d367d433e675a69ec549b11f1
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-11 00:47:27 +0200

    Update useDelay function

--- src/app/hooks/useDelay.ts
+++ src/app/hooks/useDelay.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 /**
  * useDelay hook allows you to delay execution of a callback function.
@@ -6,7 +6,7 @@ import { useCallback, useEffect, useRef } from 'react';
  * @param callback - Function to be delayed.
  * @param delay - Delay in milliseconds before the callback is executed.
  */
-export const useDelay = (callback: () => void, delay: number): () => void => {
+export const useDelay = (callback: () => void, delay: number): void => {
   // Using a ref to store the callback ensures that the latest callback is used without re-setting the timer.
   const callbackRef = useRef(callback);
 
@@ -15,8 +15,7 @@ export const useDelay = (callback: () => void, delay: number): () => void => {
     callbackRef.current = callback;
   }, [callback]);
 
-  // Set up a timeout to execute the callback after the delay.
-  return useCallback(() => {
+  useEffect(() => {
     const timer = setTimeout(() => {
       callbackRef.current();
     }, delay);
@@ -24,4 +23,4 @@ export const useDelay = (callback: () => void, delay: number): () => void => {
     // Clean up the timer when the delay or component unmounts.
     return () => clearTimeout(timer);
   }, [delay]);
-}
+};
