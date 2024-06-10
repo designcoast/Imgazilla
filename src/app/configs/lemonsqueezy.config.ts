35faf74e9c753909e5d6d74b70fd048e2bd593bb
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-11 00:47:44 +0200

    update config for payment provider

--- src/app/configs/lemonsqueezy.config.ts
+++ src/app/configs/lemonsqueezy.config.ts
@@ -1,12 +1,10 @@
 
 import { lemonSqueezySetup } from '@lemonsqueezy/lemonsqueezy.js';
 
-export function configureLemonSqueezy() {
+export const configureLemonSqueezy = () => {
   lemonSqueezySetup({
     apiKey: process.env.LEMONSQUEEZY_API_KEY,
     onError: (error) => {
-      // eslint-disable-next-line no-console -- allow logging
-      console.error(error);
       throw new Error(`Lemon Squeezy API error: ${error.message}`);
     },
   });
