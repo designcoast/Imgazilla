a380033f459fddd5c43c211be5feba20f45f9905
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-13 21:54:18 +0200

    Update Account component

--- src/app/components/shared/Account/Account.tsx
+++ src/app/components/shared/Account/Account.tsx
@@ -7,7 +7,7 @@ import {
   PopoverContent,
   PopoverTrigger,
   Button,
-  InstructionModal, EarnCreditsModal, TooltipProvider, Tooltip, TooltipTrigger, TooltipContent
+  InstructionModal, EarnCreditsModal
 } from '@/app/components';
 
 const DISCORD_URL = process.env.DISCORD_HELP_COMMUNITY_URL;
@@ -25,26 +25,13 @@ export const Account = () => {
       <Popover onOpenChange={setIsOpen}>
         <PopoverTrigger>
           <div className="flex gap-2 items-center bg-tabBG py-2 px-5 rounded-lg">
-            {/*<TooltipProvider>*/}
-            {/*  <Tooltip>*/}
-            {/*    <TooltipTrigger className="p-0 h-fit">*/}
-                  <div className="flex gap-1 items-center">
-                    <p className="font-bold text-sm">{accountDetails.credits}</p>
-                    <p className="text-sm">credits</p>
-
-                    <div className="w-[32px] h-[25px] overflow-hidden rounded-sm ml-2.5">
-                      <img src={accountDetails.photoUrl} alt={accountDetails.name}/>
-                    </div>
-                  </div>
-            {/*    </TooltipTrigger>*/}
-            {/*    <TooltipContent side="bottom">*/}
-            {/*      <p className="flex">*/}
-            {/*        Credits stack and <span className="inline-block font-bold ml-0.5">expire monthly</span>, with no*/}
-            {/*        rollover.*/}
-            {/*      </p>*/}
-            {/*    </TooltipContent>*/}
-            {/*  </Tooltip>*/}
-            {/*</TooltipProvider>*/}
+            <div className="flex gap-1 items-center">
+              <p className="font-bold text-sm">{accountDetails.credits}</p>
+              <p className="text-sm">credits</p>
+              <div className="w-[32px] h-[25px] overflow-hidden rounded-sm ml-2.5">
+                <img src={accountDetails.photoUrl} alt={accountDetails.name}/>
+              </div>
+            </div>
             {isOpen ? (
               <ChevronUp className="stroke-input"/>
             ) : (
