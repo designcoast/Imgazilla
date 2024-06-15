1dcc2d4609edf896660d7d0ddebb2e786a2190bf
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-15 23:33:47 +0200

    Update API with a new header

--- src/app/redux/api.ts
+++ src/app/redux/api.ts
@@ -1,20 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { prepareHeaders } from '@/app/lib/generateSignature';
-
-export interface ErrorResponseType {
-  data: {
-    statusCode: number;
-    message: string;
-  },
-  status: number;
-}
+import { RootState } from '@/app/redux/store';
 
 export const createBaseApi = (reducerPath: string) => createApi({
   reducerPath,
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.BASE_API_URL,
-    prepareHeaders: (headers, { endpoint }) => {
-      return prepareHeaders(headers, endpoint);
+    prepareHeaders: (headers, { endpoint, getState }) => {
+      const figmaId = (getState() as RootState)?.account?.figmaUserID || endpoint;
+      return prepareHeaders(headers, endpoint, figmaId);
     },
   }),
   endpoints: () => ({}),
