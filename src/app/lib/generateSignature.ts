083ec55b5bfb965d81df175b726753f8f192d49b
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-15 23:34:13 +0200

    Add encrypt function for a new header

--- src/app/lib/generateSignature.ts
+++ src/app/lib/generateSignature.ts
@@ -1,14 +1,27 @@
 import createHmac from 'create-hmac';
+import CryptoJS from 'crypto-js';
 
 const secret = process.env.REQUESTS_SECRET_KEY;
+
+const exceptedEndpoints = ['checkAccount', 'createAccount'];
+
+export const encrypt = (value: string): string => {
+  return CryptoJS.AES.encrypt(value, secret).toString();
+};
 
 export const generateSignature = (body: any) => {
   const hmac = createHmac('sha256', secret);
   return hmac.update(JSON.stringify(body)).digest('hex');
 };
 
-export const prepareHeaders = (headers: any, body: any) => {
-  const signature = generateSignature(body);
+export const prepareHeaders = (headers: any, endpoint: any, figmaId: string) => {
+  if (!exceptedEndpoints.includes(endpoint)) {
+    const encryptedId = encrypt(figmaId);
+    headers.set('x-Figma-id', encryptedId);
+  }
+
+  const signature = generateSignature(endpoint);
   headers.set('x-Figma-Signature', signature);
+
   return headers;
 };
