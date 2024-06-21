8d2e9699ee6801cef01b4b93a346cc923f8e5de6
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-21 21:18:57 +0200

    Update component

--- src/app/components/ui/Overlay/Overlay.tsx
+++ src/app/components/ui/Overlay/Overlay.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Loading } from '@/app/components';
 
-export const Overlay = () => {
+type Props = {
+  children?: React.ReactNode
+}
+
+export const Overlay = ({ children = <Loading/> }: Props) => {
   return (
     <div className="fixed inset-0 top-[115px] bg-black bg-opacity-50 z-50">
       <div className="flex justify-center items-center h-full">
-        <Loading/>
+        {children}
       </div>
     </div>
   )
