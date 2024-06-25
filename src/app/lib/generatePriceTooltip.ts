7c40d81e08f5adafb2538f4ee50a57eeb702e7ca
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-25 17:24:20 +0200

    Update text for tooltip component

--- src/app/lib/generatePriceTooltip.ts
+++ src/app/lib/generatePriceTooltip.ts
@@ -5,5 +5,5 @@ export const generateTooltip = (credits: number): string => {
   const faviconCount = credits / parseInt(FAVICON_ARCHIVE_CREDITS_COST);
   const optimizationCount = credits / parseInt(IMAGE_CREDITS_COST);
 
-  return `You can export ${faviconCount} archive${faviconCount > 1 ? 's' : ''} with favicon or optimize ${optimizationCount} images`;
+  return `You can export ${faviconCount} archive${faviconCount > 1 ? 's' : ''} with favicon or ${optimizationCount} archives with images`;
 };
