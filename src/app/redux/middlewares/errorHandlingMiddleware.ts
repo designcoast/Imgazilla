d9c5ea949065d11b8439c07aeb2e805ddad7613b
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-25 17:24:48 +0200

    Update error handler if account api returns 404 status code

--- src/app/redux/middlewares/errorHandlingMiddleware.ts
+++ src/app/redux/middlewares/errorHandlingMiddleware.ts
@@ -2,8 +2,22 @@ import { Middleware } from '@reduxjs/toolkit';
 import { isRejectedWithValue } from '@reduxjs/toolkit';
 import { toast, } from 'sonner';
 
-export const errorHandlingMiddleware: Middleware = () => (next) => (action: { payload: { data: { message: string } }}) => {
+type ActionType = {
+  payload: {
+    data: {
+      message: string,
+      statusCode: number,
+      path: string
+    }
+  }
+}
+
+export const errorHandlingMiddleware: Middleware = () => (next) => (action: ActionType) => {
   if (isRejectedWithValue(action)) {
+    if (action.payload.data.statusCode === 404 && action.payload.data.path.includes('getAccount')) {
+      return next(action);
+    }
+    console.log('action.payload', action.payload);
     toast.warning('Error', {
       description: action.payload.data.message
     });
