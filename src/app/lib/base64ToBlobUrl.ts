53a530f76eac1c7dba06f8f59ad73ace914dfa52
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-25 02:27:55 +0200

    Add function for updating formats

---
+++ src/app/lib/base64ToBlobUrl.ts
@@ -0,0 +1,14 @@
+import { convertToImageUrl } from '@/app/lib/convertToImageUrl';
+
+export const base64ToBlobUrl = (base64: string, type: string): string => {
+  const byteCharacters = atob(base64);
+  const byteNumbers = new Array(byteCharacters.length);
+
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+
+  const byteArray = new Uint8Array(byteNumbers);
+
+  return convertToImageUrl(byteArray, type);
+}
