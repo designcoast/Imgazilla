42c285c8fb0f536b4e5dec0b4c22b01e4c870e1b
Oleh H <oleh.husiev.dev@gmail.com>
2024-07-02 22:58:19 +0200

    Add sentry init function and app wrapper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import * as Sentry from '@sentry/react';
+
 import { ThemeProvider } from '@/app/components/theme-provider';
 import { RootLayout } from '@/app/layouts/RootLayout';
 import { ReduxProvider } from '@/app/redux/provider';
 import { AccountStatusChecker } from '@/app/HOC/AccountStatusChecker';
 import { Toaster } from '@/app/components';
 import { WithDefaultTabSetter } from '@/app/HOC/WithDefaultTabSetter';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN_LINK,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  // tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const App = () => {
   return (
@@ -26,4 +42,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default Sentry.withProfiler(App);
