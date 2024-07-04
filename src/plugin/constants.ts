d95ab3a7d4a69671367274d967ac976c072edbda
Oleh H <oleh.husiev.dev@gmail.com>
2024-07-04 17:34:40 +0200

    Update constants and root App component

--- src/plugin/constants.ts
+++ src/plugin/constants.ts
@@ -1,1 +1,6 @@
 export const RELAUNCH_DATA_STORE_KEY = 'RELAUNCH_DATA_STORE_KEY';
+
+export const FIGMA_COMMANDS_KEY = {
+  IMAGE_OPTIMIZATION: 'imagesOptimization',
+  FAVICON_EXPORT: 'faviconExport',
+}
