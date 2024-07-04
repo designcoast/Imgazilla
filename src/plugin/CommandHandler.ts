cb5e0dfa4e66d346a8d7b1194f91a7deb8714ccd
Oleh H <oleh.husiev.dev@gmail.com>
2024-07-04 17:33:54 +0200

    Update figma command handler with a new event

--- src/plugin/CommandHandler.ts
+++ src/plugin/CommandHandler.ts
@@ -1,6 +1,7 @@
 import { MessageSender } from '@/plugin/MessageSender';
 import { MessageType } from '@/plugin/FigmaUIMessaging';
 import { EventType } from '@/eventType';
+import { FIGMA_COMMANDS_KEY } from '@/plugin/constants';
 
 export class CommandHandler {
 
@@ -9,21 +10,37 @@ export class CommandHandler {
 
   constructor() {
     this.commandMap = {
-      'imagesOptimization': () => this.handleImagesOptimizationCommand(),
+      [FIGMA_COMMANDS_KEY.IMAGE_OPTIMIZATION]: () => this.handleImagesOptimizationCommand(),
+      [FIGMA_COMMANDS_KEY.FAVICON_EXPORT]: () => this.handleFaviconExportCommand(),
     };
     this.messageSender = new MessageSender();
   }
 
   public handleCommand(): void {
     const command = figma.command;
-    if (command in this.commandMap) {
-      this.commandMap[command]();
+
+    switch (command) {
+      case FIGMA_COMMANDS_KEY.IMAGE_OPTIMIZATION:
+        this.commandMap[FIGMA_COMMANDS_KEY.IMAGE_OPTIMIZATION]()
+        break;
+      default:
+        this.commandMap[FIGMA_COMMANDS_KEY.FAVICON_EXPORT]()
+        break;
     }
   }
 
   private handleImagesOptimizationCommand() {
     const message = {
       type: EventType.OPEN_IMAGES_OPTIMIZATION_TAB,
+      payload: {}
+    } satisfies MessageType
+
+    this.messageSender.sendMessageToUI(message)
+  }
+
+  private handleFaviconExportCommand() {
+    const message = {
+      type: EventType.OPEN_FAVICON_EXPORT_TAB,
       payload: {}
     } satisfies MessageType
 
