1716e6718dded311997453bafcd29482873c3dd4
Oleh H <oleh.husiev.dev@gmail.com>
2024-07-14 01:48:48 +0200

    Update Root layout with analytics

--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
 import {
@@ -14,14 +14,35 @@ import {
   IMAGE_OPTIMIZATION_TAB,
   selectDisabledTab
 } from '@/app/redux/features';
+import { useSentryAnalytics } from '@/app/hooks/useSentryAnalytics';
 
 export const RootLayout = () => {
   const name = useSelector(getActiveTab);
 
-  const disabledTab= useSelector(selectDisabledTab);
+  const sendAnalyticsEvent = useSentryAnalytics();
+
+  const disabledTab = useSelector(selectDisabledTab);
 
   const isFaviconTabDisabled = disabledTab?.name === FAVICON_TAB ?? false;
   const isImagesOptimizationTabDisabled = disabledTab?.name === IMAGE_OPTIMIZATION_TAB ?? false;
+
+  const handleOnFaviconTabClick = useCallback(() => {
+    sendAnalyticsEvent({
+      eventName: 'button_click',
+      category: 'user_interaction',
+      label: 'favicon_tab',
+      value: 1,
+    });
+  }, []);
+
+  const handleOnImagesTabClick = useCallback(() => {
+    sendAnalyticsEvent({
+      eventName: 'button_click',
+      category: 'user_interaction',
+      label: 'images_optimization_tab',
+      value: 1,
+    });
+  }, [])
 
   return (
     <>
@@ -30,8 +51,20 @@ export const RootLayout = () => {
       </div>
       <Tabs defaultValue={name} className="w-full">
         <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="favicon" disabled={isFaviconTabDisabled}>Favicon exporter</TabsTrigger>
-          <TabsTrigger value="imagesOptimization" disabled={isImagesOptimizationTabDisabled}>Image optimization</TabsTrigger>
+          <TabsTrigger
+            value="favicon"
+            disabled={isFaviconTabDisabled}
+            onClick={handleOnFaviconTabClick}
+          >
+            Favicon exporter
+          </TabsTrigger>
+          <TabsTrigger
+            value="imagesOptimization"
+            disabled={isImagesOptimizationTabDisabled}
+            onClick={handleOnImagesTabClick}
+          >
+            Image optimization
+          </TabsTrigger>
         </TabsList>
         <>
           <TabsContent value="favicon">
