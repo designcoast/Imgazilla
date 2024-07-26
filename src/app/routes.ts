4f69b70b4e9ec9254cf43ed1cc5df092f3f7728a
Oleh H <oleh.husiev.dev@gmail.com>
2024-07-27 00:13:45 +0200

    Add routes for app

---
+++ src/app/routes.ts
@@ -0,0 +1,6 @@
+export const APP_ROUTES = {
+  FAVICON_EXPORT: 'Favicon exporter',
+  IMAGE_OPTIMIZATION: 'Image optimization',
+  GIF_ANIMATION: 'GIF animation',
+  PDF_CREATION: 'PDF creation',
+}
