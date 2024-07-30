7af54f35a8c473fb9e8bfe2adb9a08f7f5fa5ad0
Oleh H <oleh.husiev.dev@gmail.com>
2024-07-31 00:48:49 +0200

    Add a new event ui types

--- src/eventType.ts
+++ src/eventType.ts
@@ -6,9 +6,12 @@ export enum EventType {
   IMAGES_UINT_ARRAY_COLLECTION = 'images_uint_array_collection',
   OPEN_IMAGES_OPTIMIZATION_TAB = 'open_images_optimization_tab',
   OPEN_FAVICON_EXPORT_TAB = 'open_favicon_export_tab',
-  IMAGE_COLLECTION_COMPLETE = 'image_collection_complete'
+  IMAGE_COLLECTION_COMPLETE = 'image_collection_complete',
+  UPDATE_PLUGIN_SETTINGS = 'update_plugin_settings'
 }
 
 export enum UIEventType {
   GET_IMAGES_UINT_ARRAY_COLLECTION = 'get_images_uint_array_collection',
+  SET_CLIENT_STORE_DATA = 'set_client_store_data',
+  GET_CLIENT_STORE_DATA = 'get_client_store_data'
 }
