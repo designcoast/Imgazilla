046605f402aa62cd568f2a7a6defd3f7808b4274
Oleh H <oleh.husiev.dev@gmail.com>
2024-09-12 21:58:53 +0300

    Update config for lint file

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({ resolvePluginsRelativeTo: __dirname });
+
+module.exports = [
+  ...compat.config({
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: 'tsconfig.json',
+      tsconfigRootDir: __dirname,
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+    root: true,
+    env: {
+      node: true,
+      jest: true,
+    },
+    ignorePatterns: [
+      'eslint.config.js',
+      'dist/**',
+      'tailwind.config.js',
+      'svgo.config.js',
+      'postcss.config.js',
+    ],
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      quotes: ['error', 'single'],
+      'jsx-quotes': ['error', 'prefer-single'],
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  }),
+];
