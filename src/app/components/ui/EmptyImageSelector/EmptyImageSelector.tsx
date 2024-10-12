ad42bbb08752689a36e6a415e7b156cf755a7276
Oleh H <oleh.husiev.dev@gmail.com>
2024-10-12 12:19:44 +0200

    Update style for favicon preview component

--- src/app/components/ui/EmptyImageSelector/EmptyImageSelector.tsx
+++ src/app/components/ui/EmptyImageSelector/EmptyImageSelector.tsx
@@ -3,7 +3,7 @@ import { ImagePreviewIcon } from '@/app/components';
 export const EmptyImageSelector = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
-      <div className='flex justify-center items-center w-previewSquareWidth h-previewSquareHeight border-dashed border-2 border-primary-darkGray mb-4'>
+      <div className='flex justify-center items-center w-previewSquareWidth h-previewSquareHeight border-dashed border rounded-md border-primary-lightGreen mb-4'>
         <ImagePreviewIcon />
       </div>
       <p className='text-center font-light text-primary text-xs'>
