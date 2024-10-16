cddd1508e549c865e6d4eda50ff52dae95302ab8
Oleh H <oleh.husiev.dev@gmail.com>
2024-10-16 23:38:20 +0200

    Update background removal slice

--- src/app/redux/features/backgroundRemovalSlice.ts
+++ src/app/redux/features/backgroundRemovalSlice.ts
@@ -2,11 +2,17 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '@/app/redux/store';
 
 interface BackgroundRemovalState {
-  imageData: Uint8Array;
+  imageData?: Uint8Array;
+  processedImageData?: Uint8Array;
+  jobId?: string;
 }
 
 const initialState = {
   imageData: undefined,
+  jobId: undefined,
+  processedImageData: undefined,
+  // isProcessing: false,
+  // isJobFinished: false,
 } satisfies BackgroundRemovalState as BackgroundRemovalState;
 
 export const backgroundRemovalSlice = createSlice({
@@ -16,10 +22,25 @@ export const backgroundRemovalSlice = createSlice({
     setSelectedImage(state, action: PayloadAction<Uint8Array>) {
       state.imageData = action.payload;
     },
+    setBackgroundRemovalJobId(state, action: PayloadAction<{ jobId: string }>) {
+      state.jobId = action.payload.jobId;
+    },
+    setBackgroundRemovalResult(
+      state,
+      action: PayloadAction<{ processedImageData: Uint8Array }>,
+    ) {
+      state.processedImageData = action.payload.processedImageData;
+    },
+    resetBackgroundRemovalState: () => initialState,
   },
 });
 
-export const getSelectedImage = (state: RootState) =>
+export const getSelectedBackgroundRemovalImage = (state: RootState) =>
   state.backgroundRemoval.imageData;
 
-export const { setSelectedImage } = backgroundRemovalSlice.actions;
+export const {
+  setSelectedImage,
+  setBackgroundRemovalJobId,
+  setBackgroundRemovalResult,
+  resetBackgroundRemovalState,
+} = backgroundRemovalSlice.actions;
