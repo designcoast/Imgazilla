0a7e58897a227bf6e0c4d13dbf400ef5687c6ec4
Oleh H <oleh.husiev.dev@gmail.com>
2024-10-17 23:18:21 +0200

    Add a new component for saving single image file

---
+++ src/app/components/ui/SaveImage/SaveImage.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback } from 'react';
+import { Button } from '@/app/components';
+
+interface SaveImageProps {
+  base64Image: string;
+  name: string;
+}
+
+export const SaveImage: React.FC<SaveImageProps> = ({ base64Image, name }) => {
+  const onSaveImageToFile = useCallback(() => {
+    const byteString = atob(base64Image.split(',')[1]);
+    const mimeString = base64Image.split(',')[0].split(':')[1].split(';')[0];
+
+    const byteArray = new Uint8Array(byteString.length);
+    for (let i = 0; i < byteString.length; i++) {
+      byteArray[i] = byteString.charCodeAt(i);
+    }
+
+    const blob = new Blob([byteArray], { type: mimeString });
+
+    const blobUrl = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = blobUrl;
+    link.download = `${name}.png`;
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(blobUrl);
+  }, [base64Image]);
+
+  return <Button onClick={onSaveImageToFile}>Download</Button>;
+};
