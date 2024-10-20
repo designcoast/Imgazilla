a7a0a78104bdf5cb0eb5affc62ec861f8c6354ab
Oleh H <oleh.husiev.dev@gmail.com>
2024-10-20 20:49:33 +0200

    Update function for credits calculation

--- src/app/lib/calculateCredits.ts
+++ src/app/lib/calculateCredits.ts
@@ -1,13 +1,17 @@
 const IMAGE_CREDITS_COST = process.env.IMAGE_CREDITS_COST;
 const FAVICON_ARCHIVE_CREDITS_COST = process.env.FAVICON_ARCHIVE_CREDITS_COST;
+const BACKGROUND_REMOVAL_COST = process.env.BACKGROUND_REMOVAL_COST;
 
 export const calculateCredits = (credits: string) => {
   const faviconArchNumb =
     parseInt(credits) / parseInt(FAVICON_ARCHIVE_CREDITS_COST);
   const imageNumb = parseInt(credits) / parseInt(IMAGE_CREDITS_COST);
+  const backgroundRemovalNumb =
+    parseInt(credits) / parseInt(BACKGROUND_REMOVAL_COST);
 
   return {
     favicon: faviconArchNumb,
     images: imageNumb,
+    backgroundRemoval: backgroundRemovalNumb,
   };
 };
